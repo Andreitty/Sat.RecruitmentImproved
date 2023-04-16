65d9013486df74d8ea6afc2f99f9a6725b61a18c
Andreidy Rosa <andreidyrosa@gmail.com>
2023-04-15 21:17:41 -0400

    Updating unit tests

--- test/Sat.Recruitment.Application.Test/UserServiceTests.cs
+++ test/Sat.Recruitment.Application.Test/UserServiceTests.cs
@@ -10,6 +10,46 @@ namespace Sat.Recruitment.Application.Test
 	[TestClass]
 	public class UserServiceTests
 	{
+		/*
+		private UserService _userService;
+		private Mock<IBaseRepository<User, int>> _mockRepository;
+		private Mock<IGifCalculationStrategyFactory> _mockStrategyFactory;
+		private Mock<ILogger<UserService>> _mockLogger;
+
+		[TestInitialize]
+		public void UserServiceTestInitialize()
+		{
+			_mockRepository = new Mock<IBaseRepository<User, int>>();
+			_mockStrategyFactory = new Mock<IGifCalculationStrategyFactory>();
+			_mockLogger = new Mock<ILogger<UserService>>();
+			_userService = new UserService(_mockRepository.Object, _mockStrategyFactory.Object, _mockLogger.Object);
+		}
+
+		[TestMethod]
+		public async Task AddUserAsync_WithAddressAndNameDuplicated_ThrowsExceptionAsync()
+		{
+			// Arrange
+			var user = new User
+			{
+				Address = "Caracas-Venezuela",
+				Name = "Andreidy Rosa"
+			};
+			_mockRepository.Setup(mr => 
+				mr.ListAsync(It.IsAny<Expression<Func<User, bool>>>()))
+				.Returns(new Task<IReadOnlyList<User>>(() => new User[1]));
+			//u => u.Name.ToLower() == user.Name.ToLower()
+			//_mockRepository.Setup(mr => 
+			//		mr.ListAsync(u => u.Address.ToLower() == user.Address.ToLower()))
+			//	.Returns(new Task<IReadOnlyList<User>>(() => new User[1]));
+
+			await _userService.AddUserAsync(user);
+
+			// Act and Assert
+			Assert.Equals(1, 1);
+			//ThrowsExceptionAsync<Exception>(() => _userService.AddUserAsync(user));
+		}
+		*/
+
 		[TestMethod]
 		public async Task GetAllUsersAsync_ReturnsEmptyList()
 		{
